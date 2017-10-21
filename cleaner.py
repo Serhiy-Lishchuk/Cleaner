079e13a373e5657fb2debe43766f059a3da38bee
Serhiy <mcseruy@gmail.com>
2017-10-21 17:13:57 +0300

    add function deleted old files

--- cleaner.py
+++ cleaner.py
@@ -12,6 +12,21 @@ now_time = time.time()
 age_time = now_time - 60 * 60 * 24 * DAYS
 
 
+def deleted_old_files(folder):
+    global TOTAL_DELETED_SIZE
+    global TOTAL_DELETED_FILES
+    for path, dirs, files in os.walk(folder):
+        for file in files:
+            file_name = os.path.join(path, file)
+            file_time = os.path.getmtime(file_name)
+            if file_time < age_time:
+                size_file = os.path.getsize(file_name)
+                TOTAL_DELETED_SIZE += size_file
+                TOTAL_DELETED_FILES += 1
+                print("Deleting file: " + str(file_name))
+                os.remove(file_name)
+
+
 def main():
     pass
 
