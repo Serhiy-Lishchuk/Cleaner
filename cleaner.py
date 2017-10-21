b4c7795f5a66b0e6d38eb06886caa2894124a299
Serhiy <mcseruy@gmail.com>
2017-10-21 16:52:17 +0300

    add cleaner file

---
+++ cleaner.py
@@ -0,0 +1,20 @@
+import os
+import time
+
+DAYS = 7
+FOLDERS = []
+
+TOTAL_DELETED_SIZE = 0
+TOTAL_DELETED_FILES = 0
+TOTAL_DELETED_DIRS = 0
+
+now_time = time.time()
+age_time = now_time - 60 * 60 * 24 * DAYS
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
